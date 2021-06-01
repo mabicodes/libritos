465bb2e0d0f1039a9f5edb0e4f0f1d9cdf76f07f
Nicole Uy <mabigaming26@gmail.com>
2021-06-01 12:04:21 +0200

    añadiendo models package y services

---
+++ src/main/java/org/factoriaf5/libritos/models/Book.java
@@ -0,0 +1,4 @@
+package org.factoriaf5.libritos.models;
+
+public class Book {
+}
---
+++ src/main/java/org/factoriaf5/libritos/models/BookRepository.java
@@ -0,0 +1,63 @@
+package org.factoriaf5.libritos.models;
+
+import org.hibernate.boot.model.relational.Sequence;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+public interface BookRepository {
+    @Entity
+    @Table(name = "books")
+    public class Book implements Serializable {
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE)
+        private Long id;
+        private String title;
+        private String author;
+        private String category;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        @Override
+        public String toString() {
+            return "Book{" +
+                    "id=" + id +
+                    ", title='" + title + '\'' +
+                    ", author='" + author + '\'' +
+                    ", category='" + category + '\'' +
+                    '}';
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public void setCategory(String category) {
+            this.category = category;
+        }
+    }
+
+
+}
