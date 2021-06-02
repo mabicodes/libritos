9078d598ce58a92dd26d39634483ea2a9bdf766e
Nicole Uy <mabigaming26@gmail.com>
2021-06-02 09:33:17 +0200

    Book.java

--- src/main/java/org/factoriaf5/libritos/models/BookRepository.java
+++ src/main/java/org/factoriaf5/libritos/models/BookRepository.java
@@ -1,63 +1,11 @@
 package org.factoriaf5.libritos.models;
 
-import org.hibernate.boot.model.relational.Sequence;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import javax.persistence.*;
-import java.io.Serializable;
-
+@Repository
 public interface BookRepository {
-    @Entity
-    @Table(name = "books")
-    public class Book implements Serializable {
-        @Id
-        @GeneratedValue(strategy = GenerationType.SEQUENCE)
-        private Long id;
-        private String title;
-        private String author;
-        private String category;
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        @Override
-        public String toString() {
-            return "Book{" +
-                    "id=" + id +
-                    ", title='" + title + '\'' +
-                    ", author='" + author + '\'' +
-                    ", category='" + category + '\'' +
-                    '}';
-        }
-
-        public String getAuthor() {
-            return author;
-        }
-
-        public void setAuthor(String author) {
-            this.author = author;
-        }
-
-        public String getCategory() {
-            return category;
-        }
-
-        public void setCategory(String category) {
-            this.category = category;
-        }
-    }
+    public interface BookRepository extends CrudRepository<Book, Long> { }
 
 
 }
---
+++ src/main/java/org/factoriaf5/libritos/services/BookService.java
@@ -0,0 +1,4 @@
+package org.factoriaf5.libritos.services;
+
+public class BookService {
+}
